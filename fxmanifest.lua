5cd99f9df7002f6f47ce97bf2f7eb7299e1ba902
TayMcKenzieSA <taylorocarroll@gmail.com>
2021-10-12 03:34:22 +1300

    Added Taxidermy Heads

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,25 @@
 fx_version 'cerulean'
-games { 'gta5' }
+game { 'gta5' }
 
 
 
-authors { 'AleBrambi45', 'TayMcKenzieNZ' }
+authors { 'AleBrambi45', 'TayMcKenzieNZ, SMGMissy' }
 description 'Ranch De Caniche'
-version '2.0.2'
+version '2.0.3'
  url 'https://github.com/TayMcKenzieNZ/Ranch-De-Caniche'
 
 
 this_is_a_map 'yes'
+
+
+
+data_file "DLC_ITYP_REQUEST" "fish_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "deerhead_ytyp.ytyp"
+
+
+
+---  Huge shoutout to SMGMissy on the FiveM forums, for allowing me permission for the  usage of the taxidermy animals, Jen#3812, https://discord.gg/Rtd59V8eqp ---
 
 ---                                                                         ---
 ---   THIS IS A FREE RESOURCE PROVIDED UPLOADED TO GITHUB FOR FIVEM USAGE   ---
